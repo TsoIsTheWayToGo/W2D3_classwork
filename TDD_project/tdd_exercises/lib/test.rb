842424942ff9f9da4d641f8be4a89f2b0d8d88ed
App Academy Student <appacademy@Apps-Mac-mini-2.local>
2018-10-31 15:06:46 -0700

    Made testes for methods

--- TDD_project/tdd_exercises/lib/test.rb
+++ TDD_project/tdd_exercises/lib/test.rb
@@ -1,4 +1,4 @@
-
+require "byebug"
   def my_uniq(arr)
     return [] if arr.length == 0 
     result = []
@@ -6,4 +6,53 @@
       result << item if !result.include?(item) 
     end
     result
+  end
+
+  class Array
+    
+    def two_sum
+      # byebug
+      return [] if self.length == 0
+      result = []
+      (self.length - 1).times do |i|
+        (i+1...self.length).each do |j|
+          if self[i] + self[j] == 0
+            result << [i,j]
+          end
+        end
+      end
+      result
+    end
+  
+  end
+
+  
+  def my_transpose(array)
+  
+    raise ArgumentError unless array.all? {|el| el.length == array[0].length}
+    
+    result = []
+    i = 0
+    while i < array.length
+      j = 0
+      temp = []
+      while j < array.length
+        temp << array[j][i]
+        j += 1
+      end
+      result << temp
+      i += 1
+    end 
+    result
+  end
+  
+  def stock_picker(array)
+    max = array.max
+    max_index = array.index(max)
+    min = array.min
+    min_index = array.index(min)
+    if max_index > min_index
+      return "you should buy on day: #{min_index + 1} and sell on day: #{max_index + 1}"
+    end
+    return nil
   end
--- TDD_project/tdd_exercises/spec/test_spec.rb
+++ TDD_project/tdd_exercises/spec/test_spec.rb
@@ -9,3 +9,40 @@ describe "#my_uniq" do
     expect(my_uniq([1,1,6,0])).to eq([1,6,0])
   end
 end
+
+RSpec.describe "Array" do
+  describe "#two_sum" do
+    it "Returns an empty array if self is empty" do
+      expect([].two_sum).to eq([])
+    end
+    
+    it "finds all pairs of positions where the elements at those positions sum to zero." do
+      expect([-1, 0, 2, -2, 1].two_sum).to eq([[0,4],[2,3]])
+    end
+    
+    it "it returns pairs with smaller index first" do
+      expect([-1,1,-2,0,2].two_sum).to eq([[0,1],[2,4]])
+    end
+  end
+end
+
+describe "#my_transpose" do
+  it "raises an error if nested arrays are not the same lenth" do
+    test = [[0, 1, 2],[3, 4, 5],[6, 7, 8,1]]
+    expect {my_transpose(test)}.to raise_error(ArgumentError)
+  end
+  it "returns an array of columns" do
+    test = [[0, 1, 2],[3, 4, 5],[6, 7, 8]]
+    test_result = [[0, 3, 6],[1, 4, 7],[2, 5, 8]]
+    expect(my_transpose(test)).to eq(test_result)
+  end 
+end
+
+describe "#stock_picker" do
+  it "return nil if there if prices are declining" do 
+    expect(stock_picker([15,14,13,12,11])).to eq(nil)
+  end
+  it "returns the days as indices when you should buy and sell stocks" do
+  expect(stock_picker([5,21,5,15,20,25])).to eq("you should buy on day: 1 and sell on day: 6")
+  end
+end
