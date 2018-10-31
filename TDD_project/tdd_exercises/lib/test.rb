37903ca4e9c6fc9475c0e67ac5520506b4389629
App Academy Student <appacademy@Apps-Mac-mini-2.local>
2018-10-31 12:50:07 -0700

    Make specs for uniq method

--- TDD_project/tdd_exercises/lib/test.rb
+++ TDD_project/tdd_exercises/lib/test.rb
@@ -1,10 +1,9 @@
 
   def my_uniq(arr)
-    return [] if arr.length == 0
+    return [] if arr.length == 0 
     result = []
     arr.each do |item|
       result << item if !result.include?(item) 
-      
     end
     result
   end
